a9f1be345d8cad4df5faef98563edf881ef015c6
evizitei <ethan.vizitei@gmail.com>
2010-10-22 09:10:16 -0500

    revise signup form
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,48 @@
+// JavaScript Document
+
+// execute your scripts when the DOM is ready. this is mostly a good habit
+$(document).ready(function() {
+
+	// initialize scrollable
+ 	$(".scrollable").scrollable({speed: 500});
+
+});
+
+$(function() {
+	$(".lightbox").fancybox({
+		'titleShow'		: false,
+		'transitionIn'	: 'elastic',
+		'transitionOut'	: 'elastic'
+	});
+	
+});
+
+
+
+Cufon.replace('.sidebar-title', { fontFamily: 'eurofurence' });
+Cufon.replace('.menu ul li a', { fontFamily: 'eurofurence' , hover:true});
+Cufon.replace('.page h1', { fontFamily: 'eurofurence' });
+Cufon.replace('.page h2', { fontFamily: 'eurofurence' });
+Cufon.replace('.page h3', { fontFamily: 'eurofurence' });
+Cufon.replace('.about-us', { fontFamily: 'eurofurence' });
+Cufon.replace('.what-we-do', { fontFamily: 'eurofurence' });
+Cufon.replace('.footer-title', { fontFamily: 'eurofurence' });
+Cufon.replace('.newsletter-title', { fontFamily: 'eurofurence' });
+Cufon.replace('.stalk-us p', { fontFamily: 'eurofurence' });
+Cufon.replace('.project-name', { fontFamily: 'eurofurence' });
+Cufon.replace('.tagline-text', { fontFamily: 'eurofurence' });
+Cufon.replace('.service-header', { fontFamily: 'eurofurence' });
+Cufon.replace('.btn-order-now', { fontFamily: 'eurofurence' });
+Cufon.replace('.btn-try-it-now', { fontFamily: 'eurofurence' });
+Cufon.replace('.btn-submit', { fontFamily: 'eurofurence' });
+
+
+
+
+
+
+
+
+
+
+
