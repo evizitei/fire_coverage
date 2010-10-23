6fa4e0fc3b957183ad4bf312cd8e1fe0841aeaa7
evizitei <ethan.vizitei@gmail.com>
2010-10-23 12:17:52 -0500

    remove forgery protection from sms controller
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,4 +1,5 @@
 class SmsController < ApplicationController
+  protect_from_forgery :except=>:incoming_sms
   def incoming_sms
     user = User.find_by_phone(params[:device_address])
     sms = Moonshado::Sms.new(user.phone, user.staffing_message_for(params[:message]))
