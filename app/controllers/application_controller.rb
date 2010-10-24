30d9463d289eb51e536bdad9b821909316fdefbc
evizitei <ethan.vizitei@gmail.com>
2010-10-24 09:26:43 -0500

    set timezone for app
    Signed-off-by: evizitei <ethan.vizitei@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
+  before_filter :set_timezone
   protect_from_forgery
+  
+  def set_timezone
+    Time.zone = "America/Chicago"
+  end
 end
